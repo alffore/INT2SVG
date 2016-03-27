eeed898acfd75b5da59dcf906847fcb6903a0199
A. Alfonso F. R <alffore@gmail.com>
2016-03-26 23:26:00 -0600

    Se modifican los escripts para incluir una dimensión adicional y simplificar la construcción y copia de los mismos

--- bin/test.sh
+++ bin/test.sh
@@ -6,42 +6,14 @@ make clean
 for i in {1..32}
 do
 
+./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 1270 750 ./salidas1270/$i.svg ./bin/excepcion_mun.txt
+
 ./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 950 750 ./salidas950/$i.svg ./bin/excepcion_mun.txt
-
-done
-
-
-for i in {1..32}
-do
 
 ./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 714 750 ./salidas714/$i.svg ./bin/excepcion_mun.txt
 
-done
-
-
-for i in {1..32}
-do
-
 ./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 590 600 ./salidas590/$i.svg ./bin/excepcion_mun.txt
-
-done
-
-
-for i in {1..32}
-do
 
 ./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 334 400 ./salidas334/$i.svg ./bin/excepcion_mun.txt
 
 done
-
-cd ./salidas950
-find ./ -size 0b -delete
-
-cd ../salidas714
-find ./ -size 0b -delete
-
-cd ../salidas590
-find ./ -size 0b -delete
-
-cd ../salidas334
-find ./ -size 0b -delete
--- bin/test_nal.sh
+++ bin/test_nal.sh
@@ -3,6 +3,15 @@ make all
 make clean
 
 
+./bin/int2svg.exe ../INTS_MEXICO/mge2013v6_2.int 3 3 0 1270 750 ./salidas1270/0.svg
+
+
+
+cd salidas1270
+find ./ -size 0b -delete
+
+
+cd ..
 
 ./bin/int2svg.exe ../INTS_MEXICO/mge2013v6_2.int 3 3 0 950 750 ./salidas950/0.svg
 
@@ -47,7 +56,8 @@ find ./ -size 0b -delete
 
 
 cd ..
-cp salidas334/0.* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/334/ -v
-cp salidas590/0.* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/590/ -v
-cp salidas714/0.* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/714/ -v
-cp salidas950/0.* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/950/ -v
+cp salidas334/* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/334/ -v
+cp salidas590/* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/590/ -v
+cp salidas714/* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/714/ -v
+cp salidas950/* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/950/ -v
+cp salidas1270/* $HOME/NetBeansProjects/nusic/NUSiC/mapasng/origen/1270/ -v
