5d617c07f2b07f1af552146141d790f1981ef65c
A. Alfonso F. R <alffore@gmail.com>
2016-04-02 10:11:38 -0600

    Se completa el generadoir cartografico a escala adecuada

--- bin/test.sh
+++ bin/test.sh
@@ -6,14 +6,14 @@ make clean
 for i in {1..32}
 do
 
-./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 1270 750 ./salidas1270/$i.svg ./bin/excepcion_mun.txt
+./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 1270 750 ./salidas1270/$i.svg ./bin/excepcion_mun.txt 1
 
-./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 950 750 ./salidas950/$i.svg ./bin/excepcion_mun.txt
+./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 950 750 ./salidas950/$i.svg ./bin/excepcion_mun.txt 1
 
-./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 714 750 ./salidas714/$i.svg ./bin/excepcion_mun.txt
+./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 714 750 ./salidas714/$i.svg ./bin/excepcion_mun.txt 1
 
-./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 590 600 ./salidas590/$i.svg ./bin/excepcion_mun.txt
+./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 590 600 ./salidas590/$i.svg ./bin/excepcion_mun.txt 1
 
-./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 334 400 ./salidas334/$i.svg ./bin/excepcion_mun.txt
+./bin/int2svg.exe ../INTS_MEXICO/mgm2013v6_2.int 3 4 $i 334 400 ./salidas334/$i.svg ./bin/excepcion_mun.txt 1
 
 done
